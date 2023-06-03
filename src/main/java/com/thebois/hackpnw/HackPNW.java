823d2e57ee572faf661918c29e9dc1b65ab44d6c
wilty <wilty@10.18.2.93>
2023-06-03 09:34:23 -0700

    setup

---
+++ src/main/java/com/thebois/hackpnw/HackPNW.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.thebois.hackpnw;
+
+/**
+ *
+ * @author wilty
+ */
+public class HackPNW {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

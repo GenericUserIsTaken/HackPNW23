d03cd901c600a52885f2bcb4cc0722f99c0d80d5
wilty <wilty@10.18.2.93>
2023-06-03 10:52:01 -0700

    setup3

--- src/main/java/com/thebois/hackpnw/HackPNW.java
+++ src/main/java/com/thebois/hackpnw/HackPNW.java
@@ -13,5 +13,6 @@ public class HackPNW {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //just a new line for setup2
+        //test commit3
     }
 }
